e2da5db7e1c1619d525fd0ee159bd92794c4103a
kaito <mokos.cook@gmail.com>
2021-12-16 17:08:42 +0900

    twitterリンク設定

--- components/Headcom.js
+++ components/Headcom.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import Head from 'next/head'
 
-function Headcom() {
+function Headcom(props) {
     return (
         <Head>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
             <link href="https://fonts.googleapis.com/css2?family=Readex+Pro:wght@200;300;400;500;600;700&display=swap" rel="stylesheet" />
-            <script></script>
+            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:site" content="@zonyzony14" />
+            <meta name="twitter:title" content="Dipitto Blog" />
+            <meta name="twitter:image" content="/images/link_img.jpg" />
         </Head>
     )
 }
