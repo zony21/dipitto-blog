78ce19460f4df1723c6a9707a10110b69d1bbfab
kaito <mokos.cook@gmail.com>
2021-12-21 12:15:29 +0900

    yarnの修正

--- components/Headcom.js
+++ components/Headcom.js
@@ -5,7 +5,7 @@ function Headcom(props) {
     return (
         <Head>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+            <link rel="preconnect" href="https://fonts.gstatic.com" Crossorigin />
             <link href="https://fonts.googleapis.com/css2?family=Readex+Pro:wght@200;300;400;500;600;700&display=swap" rel="stylesheet" />
             <meta name="twitter:card" content="summary" />
             <meta name="twitter:site" content="@zonyzony14" />
